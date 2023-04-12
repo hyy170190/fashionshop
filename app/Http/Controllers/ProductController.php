6ad9071bf127ccda1b2021b526600cb14c83bb45
Low Wei Jie <weijie0521@gmail.com>
2023-04-12 16:15:48 +0800

    WJ_Admin_Committed

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,4 +3,132 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Transaction;
 
+
+use Session;
+
+class ProductController extends Controller
+{
+    //
+    public function index()
+    {
+
+        if (Gate::allows('isAdmin')) {
+            $data = Product::getRecord();
+            return view('product.index', ['products' => $data]);
+        } 
+        else {
+            return abort('403');
+        }
+    }
+
+    public function create(Request $request)
+    {
+        $data = product::getRecord();
+
+        if (Gate::allows('isAdmin')) {
+            if ($request->isMethod('POST')) {
+
+                $request->validate([
+                    'product_name' => 'required',
+                    'type_name' => 'required',
+                    'description' => 'required',
+                    'price' => 'required|numeric',
+                    'quantity' => 'required|numeric',
+                ]);
+
+                Product::create($request->all());
+                return view('product.index', ['products' => $data])->with('success', 'product created successfully.');
+            }
+            return view('product.create');
+        } else {
+            return abort('403');
+
+        }
+    }
+
+    // public function buy(Request $request, $product_id)
+    // {
+    //     $data = Product::getRecord();
+    //     $user = Auth::user();
+
+    //     $product = Product::find($product_id);
+
+    //     if (Gate::allows('isUser')) {
+    //         if ($request->isMethod('POST')) {
+    //             $request->validate([
+    //                 'quantity' => 'required|numeric',
+    //             ]);
+    //             Transaction::create([
+    //                 'user_id'=>$user->id,
+    //                 'product_id'=>$product_id,
+    //                 'quantity'=>$request('quantity'),
+    //                 'total'=>($request('quantity') * $request('price')),
+    //             ]);
+    //             $product->update([
+    //                 'quantity'=>($product('quantity') - $request('quantity')),
+    //             ]);
+
+    //             return view('product.index', ['products' => $data])->with('success', 'product bought successfully.');
+    //         }
+    //         return view('product.buy', [
+    //             'product' => $product,
+    //             'id' => $product->id,
+    //         ]);
+    //     } else {
+    //         return abort('403');
+
+    //     }
+    // }
+
+    public function update(Request $request, $product_id)
+    {
+        $data = Product::getRecord();
+
+        $product = Product::find($product_id);
+
+        if (Gate::allows('isAdmin')) {
+            if ($request->isMethod('POST')) {
+
+                $request->validate([
+                    'product_name' => 'required',
+                    'type_name' => 'required',
+                    'description' => 'required',
+                    'price' => 'required|numeric',
+                    'quantity' => 'required|numeric',
+                ]);
+                $product->update($request->all());
+                return view('product.index', ['products' => $data])->with('success', 'product created successfully.');
+            }
+            return view('product.update', [
+                'product' => $product,
+                'id' => $product->id,
+            ]);
+        } else {
+            return abort('403');
+
+        }
+    }
+
+    public function delete($product_id)
+    {
+        $data = Product::getRecord();
+        $product = Product::find($product_id);
+
+        if (Gate::allows('isAdmin')) {
+
+            $product->delete();
+
+            return view('product.index', [
+                'products' => $data,
+            ])->with('success', 'product delete successfully.');
+        } else {
+            return abort('403');
+
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,9 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
 
+/* This is product route */
+Route::match(['get', 'post'],'/product/index', [ProductController::class, 'index'])->name('product.index');
+Route::match(['get', 'post'],'/product/create', [ProductController::class, 'create'])->name('product.create');
+// Route::match(['get', 'post'],'/product/buy{id}', [ProductController::class, 'buy'])->name('product.buy');
+Route::match(['get', 'post'],'/product/update{id}', [ProductController::class, 'update'])->name('product.update');
+Route::match(['get', 'post'],'/product/delete{id}', [ProductController::class, 'delete'])->name('product.delete');
