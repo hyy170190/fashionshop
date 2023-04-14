650da6e2abe5205f029b2946a7652c93ab177ae5
hyy170190 <70197056+hyy170190@users.noreply.github.com>
2023-04-14 12:58:53 +0800

    Update database 1/2

--- database/seeders/InventorySeeder.php
+++ database/seeders/InventorySeeder.php
@@ -48,7 +48,7 @@ class InventorySeeder extends Seeder
                 'size_M' => '3',
                 'size_L' => '5',
                 'unit_price' => '334.99',
-                'category' => 'Dress',
+                'category' => 'Top',
                 'status' => true,
                 'description' => 'Kyrie signature clothing line captures the playmaker off-court style from a Nike Basketball point of view.Made from choice materials and colours, the Kyrie Pullover Hoodie uses a soft, cosy fleece with a lined hood and an adjustable hem.Printed artwork symbolises Kyrie journey.'
             ],[
@@ -57,7 +57,7 @@ class InventorySeeder extends Seeder
                 'size_M' => '2',
                 'size_L' => '3',
                 'unit_price' => '278.99',
-                'category' => 'Dress',
+                'category' => 'Top',
                 'status' => true,
                 'description' => 'Go after your hard-earned miles in soft, brushed-back comfort. The Nike Air Mid Layer delivers sweat-wicking comfort with a mesh fabric for miles of breathability. When a chill hits your route, adjustable thumbholes extend your coverage. Wear it as a layering piece or on its own with Nike Air leggings to complete the look.'
             ],[
@@ -66,7 +66,7 @@ class InventorySeeder extends Seeder
                 'size_M' => '5',
                 'size_L' => '2',
                 'unit_price' => '999.99',
-                'category' => 'Dress',
+                'category' => 'Top',
                 'status' => true,
                 'description' => 'There is beauty in imperfections. We crafted this mid-weight jumper with nested textures and a loose-knit construction to celebrate the irregularities that yield stunning results. Relief jacquard details break up the ribbed design, adding shape and highlighting the meticulous craftsmanship.'
             ],[
@@ -75,7 +75,7 @@ class InventorySeeder extends Seeder
                 'size_M' => '2',
                 'size_L' => '2',
                 'unit_price' => '238.99',
-                'category' => 'Bottom',
+                'category' => 'Top',
                 'status' => true,
                 'description' => 'NOCTA offers some of the most recognisable and uniform silhouettes. With this NOCTA Golf Polo, you will have a relaxed, easy-to-wear knit design with a breathable, course-ready feel. Made to keep you looking pristine from the first tee to the final green, it is an elevated take on a functional golf essential.'
             ],[
@@ -84,7 +84,7 @@ class InventorySeeder extends Seeder
                 'size_M' => '6',
                 'size_L' => '2',
                 'unit_price' => '198.99',
-                'category' => 'Bottom',
+                'category' => 'Top',
                 'status' => true,
                 'description' => 'Elevate your essentials in this lightweight, sweat-wicking polo. The colourful, repeating shield logo print is a nod to the history of Nike Golf. This product is made from 100% recycled polyester fibres.'
             ]
